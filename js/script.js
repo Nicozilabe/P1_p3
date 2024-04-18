f53283e3a0c6124002b287c06161665078cf491b
Nicozilabe <nicozilabe@hotmail.com>
2024-04-18 08:50:31 -0300

    fin

--- js/script.js
+++ js/script.js
@@ -11,6 +11,12 @@ function inicio() {
     document.querySelector("#btnEj8").addEventListener("click", ej8);
     document.querySelector("#btnEj9").addEventListener("click", ej9);
     document.querySelector("#btnEj10").addEventListener("click", ej10);
+    document.querySelector("#btnEj11").addEventListener("click", ej11);
+    document.querySelector("#btnEj12").addEventListener("click", ej12);
+    document.querySelector("#btnEj13").addEventListener("click", ej13);
+    document.querySelector("#btnEj14").addEventListener("click", ej14);
+    document.querySelector("#btnEj15").addEventListener("click", ej15);
+    document.querySelector("#btnEj16").addEventListener("click", ej16);
 }
 
 function ej1() {
@@ -112,13 +118,103 @@ function ej10() {
     let numero1 = parseInt(document.querySelector("#txt1Ej10").value);
     let numero2 = parseInt(document.querySelector("#txt2Ej10").value);
     let resultado;
-    if (numero1===numero2){
-        resultado=numero1;
-    }else {
-        resultado=1;
+    if (numero1 === numero2) {
+        resultado = numero1;
+    } else {
+        resultado = 1;
     }
     for (i = numero1; i <= numero2; i++) {
-            resultado *= i
+        resultado *= i
     }
     document.querySelector("#resEj10").innerHTML = resultado;
+}
+
+function ej11() {
+    let numero1 = parseInt(document.querySelector("#txt1Ej11").value);
+    let numero2 = parseInt(document.querySelector("#txt2Ej11").value);
+    let numero3 = parseInt(document.querySelector("#txt3Ej11").value);
+    let resultado = "",
+        aux;
+    if (numero1 > numero2) {
+        aux = numero1;
+        numero1 = numero2;
+        numero2 = aux;
+    }
+    for (i = numero1; i <= numero2; i++) {
+        if ((i % numero3) === 0) {
+            resultado += i + " "
+        }
+    }
+    document.querySelector("#resEj11").innerHTML = resultado;
+}
+
+function ej12() {
+    let numero1 = parseInt(document.querySelector("#txt1Ej12").value);
+    let resultado = 1;
+
+    for (i = numero1; i > 0; i--) {
+        resultado = (resultado * i)
+    }
+    document.querySelector("#resEj12").innerHTML = resultado;
+}
+
+function ej13() {
+    let alto = parseInt(document.querySelector("#txt1Ej13").value);
+    let ancho = parseInt(document.querySelector("#txt2Ej13").value);
+    let resultado = "";
+    for (let i = 1; i <= alto; i++) {
+        for (let i = 1; i <= ancho; i++) {
+            resultado += "⬛"
+        }
+        resultado += "<br>"
+    }
+    document.querySelector("#resEj13").innerHTML = resultado;
+}
+
+function ej14() {
+    let numero = parseInt(document.querySelector("#txt1Ej14").value);
+    let resultado = 1;
+    while ((numero / 10) >= 1) {
+        resultado += 1;
+        numero /= 10;
+    }
+    document.querySelector("#resEj14").innerHTML = resultado;
+}
+
+function ej15() {
+    let numero = parseInt(document.querySelector("#txt1Ej15").value);
+    let resultado = "";
+    if (numero > 2000) {
+        while (numero >= 100) {
+            numero /= 20
+        }
+        resultado = numero.toFixed(3)
+        document.querySelector("#resEj15").innerHTML = resultado;
+    } else {
+        alert(numero + " no es un número válido, debe ser mayor a 2000")
+        document.querySelector("#txt1Ej15").value = ""
+    }
+}
+
+function ej16() {
+    let resultado = 0;
+    let numero1 = parseInt(document.querySelector("#txt1Ej16").value);
+    let numero2 = parseInt(document.querySelector("#txt2Ej16").value);
+    if (numero1 > numero2) {
+        aux = numero1;
+        numero1 = numero2;
+        numero2 = aux;
+    }
+    for (i = numero1; i <= numero2; i++) {
+        if (((i % 4) === 0) && ((i % 6) === 0)) {
+            resultado = i
+            i = (numero2 + 1)
+        }
+    }
+    if (resultado === 0) {
+        document.querySelector("#resEj16").innerHTML = "No hay";
+    } else {
+        document.querySelector("#resEj16").innerHTML = resultado;
+    }
+
 }
